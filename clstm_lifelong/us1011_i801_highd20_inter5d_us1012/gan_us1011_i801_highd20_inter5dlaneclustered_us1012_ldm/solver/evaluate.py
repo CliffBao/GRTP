85427c512b321b517e6c5685cbce386016486d52
CliffBao <baopeng.ustc@gmail.com>
2021-11-08 12:59:06 +0800

    commit us1012

---
+++ clstm_lifelong/us1011_i801_highd20_inter5d_us1012/gan_us1011_i801_highd20_inter5dlaneclustered_us1012_ldm/solver/evaluate.py
@@ -0,0 +1,102 @@
+from __future__ import print_function
+import torch
+from model import highwayNet
+from utils import ngsimDataset,maskedNLL,maskedMSETest,maskedNLLTest
+from torch.utils.data import DataLoader
+import time
+import scipy.io as sio
+import numpy
+
+## Network Arguments
+args = {}
+args['use_cuda'] = True
+args['encoder_size'] = 256
+args['decoder_size'] = 256
+args['in_length'] = 16
+args['out_length'] = 25
+args['grid_size'] = (13,3)
+args['soc_conv_depth'] = 64
+args['conv_3x1_depth'] = 16
+args['dyn_embedding_size'] = 64
+args['input_embedding_size'] = 64
+args['num_lat_classes'] = 3
+args['num_lon_classes'] = 2
+args['use_maneuvers'] = False
+args['train_flag'] = False
+
+# Evaluation metric:
+# metric = 'nll'  #or rmse
+metric = 'rmse'
+
+# Initialize network
+net = highwayNet(args)
+net.load_state_dict(torch.load('trained_models/cslstm_us1011_i801_highd20p_inter5d_us1012_bestval_sgan702_us1012cond.tar'))
+# net.load_state_dict(torch.load('trained_models/cslstm_ngsim_nei4_besttr.tar'))
+if args['use_cuda']:
+    net = net.cuda()
+# tsSet_name = '../data/TestSet-us1011-gan-smooth-nei3&4-nsl.mat'
+# tsSet_name = '../data/TestSet-i801-gan-smooth-nei3&4-nsl.mat'
+# tsSet_name = '../data/TestSet-highd20-gan-pos-nsl.mat'
+# tsSet_name = '../data/TestSet-inter5d-gan-lane-clustered.mat'
+tsSet_name = '../data/TestSet-us1012-gan-smooth-nei3&4-nsl.mat'
+tsSet = ngsimDataset(tsSet_name)
+tsDataloader = DataLoader(tsSet,batch_size=128,shuffle=True,num_workers=4,collate_fn=tsSet.collate_fn)
+
+lossVals = torch.zeros(args['out_length']).cuda()
+counts = torch.zeros(args['out_length']).cuda()
+
+
+for i, data in enumerate(tsDataloader):
+    st_time = time.time()
+    hist, nbrs, mask, lat_enc, lon_enc, fut, op_mask,scale,index_division = data
+
+    # Initialize Variables
+    if args['use_cuda']:
+        hist = hist.cuda()
+        nbrs = nbrs.cuda()
+        mask = mask.cuda()
+        lat_enc = lat_enc.cuda()
+        lon_enc = lon_enc.cuda()
+        fut = fut.cuda()
+        op_mask = op_mask.cuda()
+        scale = scale.cuda()
+    if metric == 'nll':
+        # Forward pass
+        if args['use_maneuvers']:
+            fut_pred, lat_pred, lon_pred = net(hist, nbrs, mask, lat_enc, lon_enc,index_division)
+            l,c = maskedNLLTest(fut_pred, lat_pred, lon_pred, fut, op_mask)
+        else:
+            fut_pred = net(hist, nbrs, lat_enc, lon_enc,index_division)
+            l, c = maskedNLLTest(fut_pred, 0, 0, fut, op_mask,use_maneuvers=False)
+    else:
+        # Forward pass
+        if args['use_maneuvers']:
+            fut_pred, lat_pred, lon_pred = net(hist, nbrs, mask, lat_enc, lon_enc,index_division)
+            fut_pred_max = torch.zeros_like(fut_pred[0])
+            for k in range(lat_pred.shape[0]):
+                lat_man = torch.argmax(lat_pred[k, :]).detach()
+                lon_man = torch.argmax(lon_pred[k, :]).detach()
+                indx = lon_man*3 + lat_man
+                fut_pred_max[:,k,:] = fut_pred[indx][:,k,:]
+            l, c = maskedMSETest(fut_pred_max, fut, op_mask)
+
+        else:
+            fut_pred = net(hist, nbrs, lat_enc, lon_enc,index_division)
+            fut = fut*scale
+            fut_pred[:, :, 0:2] = fut_pred[:, :, 0:2] * scale
+            l, c = maskedMSETest(fut_pred, fut, op_mask)
+            if i == 0:
+                sio.savemat('cslstm-eval.mat',
+                            {'real':fut.detach().cpu().numpy(),'pred':fut_pred.detach().cpu().numpy()})
+    # print(l)
+    lossVals +=l.detach()
+    counts += c.detach()
+unit_scale = 0.3048
+if 'highd' in tsSet_name or 'inter5d' in tsSet_name:
+    unit_scale = 1.0
+if metric == 'nll':
+    print(lossVals / counts)
+else:
+    print(torch.pow(lossVals / counts,0.5)*unit_scale)   # Calculate RMSE and convert from feet to meters
+
+
